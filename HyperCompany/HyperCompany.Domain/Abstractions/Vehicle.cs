8ea150919f6640000e09ba8b0a1a62a576e01ee2
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-08-09 14:01:58 +0300

    rev

--- HyperCompany/HyperCompany.Domain/Abstractions/Vehicle.cs
+++ HyperCompany/HyperCompany.Domain/Abstractions/Vehicle.cs
@@ -1,10 +1,11 @@
-﻿namespace HyperCompany.Domain.Abstractions;
+﻿using HyperCompany.Domain.Enums;
+
+namespace HyperCompany.Domain.Abstractions;
 public abstract class Vehicle
 {
-    
+
     public Guid Id { get; set; }
-    protected Vehicle()
-    {
-        Id = Guid.NewGuid();
-    }
+
+    public required VehicleEnum Color { get; set; }
+
 }
