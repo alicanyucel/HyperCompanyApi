248092b5ec026bb965f8cb27cffc631f41cf8a94
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-08-10 19:19:39 +0300

    ..

--- HyperCompany/HyperCompany.Infrastructure/Repositories/UserRoleRepository.cs
+++ HyperCompany/HyperCompany.Infrastructure/Repositories/UserRoleRepository.cs
@@ -1,12 +1,9 @@
-﻿
-
-using GenericRepository;
+﻿using GenericRepository;
 using HyperCompany.Domain.Entities;
 using HyperCompany.Domain.Repositories;
 using HyperCompany.Infrastructure.Context;
 
 namespace HyperCompany.Infrastructure.Repositories;
-
 internal sealed class UserRoleRepository : Repository<AppUserRole, ApplicationDbContext>, IUserRoleRepository
 {
     public UserRoleRepository(ApplicationDbContext context) : base(context)
